7b2163cd3b348ad22e22d8b6165d23544690fb69
Elsie <ojemba@Ojembas-MacBook-Pro.local>
2025-03-10 12:39:27 +0200

    learning classes and methods

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+
+// Console.WriteLine("Hello, C#!");
+
+// Random dice = new();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+// first code challenge
+
+int firstValue = 500;
+int secondValue = 600;
+
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
